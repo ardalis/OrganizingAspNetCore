fa730ede9908beaffa32e8ca0b15fd0dfa77496e
Steve Smith <steve@kentsmiths.com>
2017-05-21 12:08:52 -0400

    Adding extensions methods to configure feature folders

--- src/WithFeatureFolders/Startup.cs
+++ src/WithFeatureFolders/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -35,35 +36,9 @@ namespace WithFeatureFolders
             services.AddMvc(o => o.Conventions.Add(new FeatureConvention()))
                 .AddRazorOptions(options =>
                 {
-                    // {0} - Action Name
-                    // {1} - Controller Name
-                    // {2} - Area Name
-                    // {3} - Feature Name
-                    options.AreaViewLocationFormats.Clear();
-                    options.AreaViewLocationFormats.Add("/Areas/{2}/Features/{3}/{1}/{0}.cshtml");
-                    options.AreaViewLocationFormats.Add("/Areas/{2}/Features/{3}/{0}.cshtml");
-                    options.AreaViewLocationFormats.Add("/Areas/{2}/Features/Shared/{0}.cshtml");
-                    options.AreaViewLocationFormats.Add("/Areas/Shared/{0}.cshtml");
+                    options.ConfigureFeatureFolders();
+                    // options.ConfigureFeatureFoldersSideBySideWithStandardViews();
 
-                    // replace normal view location entirely
-                    options.ViewLocationFormats.Clear();
-                    options.ViewLocationFormats.Add("/Features/{3}/{1}/{0}.cshtml");
-                    options.ViewLocationFormats.Add("/Features/{3}/{0}.cshtml");
-                    options.ViewLocationFormats.Add("/Features/Shared/{0}.cshtml");
-
-                    // add support for features side-by-side with /Views
-                    // (do NOT clear ViewLocationFormats)
-                    //options.ViewLocationFormats.Insert(0, "/Features/Shared/{0}.cshtml");
-                    //options.ViewLocationFormats.Insert(0, "/Features/{3}/{0}.cshtml");
-                    //options.ViewLocationFormats.Insert(0, "/Features/{3}/{1}/{0}.cshtml");
-                    //
-                    // (do NOT clear AreaViewLocationFormats)
-                    //options.AreaViewLocationFormats.Insert(0, "/Areas/{2}/Features/Shared/{0}.cshtml");
-                    //options.AreaViewLocationFormats.Insert(0, "/Areas/{2}/Features/{3}/{0}.cshtml");
-                    //options.AreaViewLocationFormats.Insert(0, "/Areas/{2}/Features/{3}/{1}/{0}.cshtml");
-
-
-                    options.ViewLocationExpanders.Add(new FeatureViewLocationExpander());
                 });
 
             services.AddScoped<IRepository<Ninja>, EfRepository<Ninja>>();
@@ -96,6 +71,53 @@ namespace WithFeatureFolders
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+        }
+    }
+
+    public static class RazerExtensions
+    {
+        public static void ConfigureFeatureFolders(this RazorViewEngineOptions options)
+        {
+            // {0} - Action Name
+            // {1} - Controller Name
+            // {2} - Area Name
+            // {3} - Feature Name
+            options.AreaViewLocationFormats.Clear();
+            options.AreaViewLocationFormats.Add("/Areas/{2}/Features/{3}/{1}/{0}.cshtml");
+            options.AreaViewLocationFormats.Add("/Areas/{2}/Features/{3}/{0}.cshtml");
+            options.AreaViewLocationFormats.Add("/Areas/{2}/Features/Shared/{0}.cshtml");
+            options.AreaViewLocationFormats.Add("/Areas/Shared/{0}.cshtml");
+
+            // replace normal view location entirely
+            options.ViewLocationFormats.Clear();
+            options.ViewLocationFormats.Add("/Features/{3}/{1}/{0}.cshtml");
+            options.ViewLocationFormats.Add("/Features/{3}/{0}.cshtml");
+            options.ViewLocationFormats.Add("/Features/Shared/{0}.cshtml");
+
+            options.ViewLocationExpanders.Add(new FeatureViewLocationExpander());
+        }
+
+        public static void ConfigureFeatureFoldersSideBySideWithStandardViews(this RazorViewEngineOptions options)
+        {
+            // {0} - Action Name
+            // {1} - Controller Name
+            // {2} - Area Name
+            // {3} - Feature Name
+
+            // add support for features side-by-side with /Views
+            // (do NOT clear ViewLocationFormats)
+            options.ViewLocationFormats.Insert(0, "/Features/Shared/{0}.cshtml");
+            options.ViewLocationFormats.Insert(0, "/Features/{3}/{0}.cshtml");
+            options.ViewLocationFormats.Insert(0, "/Features/{3}/{1}/{0}.cshtml");
+
+            //(do NOT clear AreaViewLocationFormats)
+            options.AreaViewLocationFormats.Insert(0, "/Areas/{2}/Features/Shared/{0}.cshtml");
+            options.AreaViewLocationFormats.Insert(0, "/Areas/{2}/Features/{3}/{0}.cshtml");
+            options.AreaViewLocationFormats.Insert(0, "/Areas/{2}/Features/{3}/{1}/{0}.cshtml");
+
+
+
+            options.ViewLocationExpanders.Add(new FeatureViewLocationExpander());
         }
     }
 }
